762308e6e8388f1f112dffa51ed4b5c17644ae19
DmitryMurashov <dmtr.murashov@gmail.com>
2024-02-25 12:14:14 +0300

    [:setup] Build |

--- setup.py
+++ setup.py
@@ -1,28 +1,23 @@
 from setuptools import setup
 
+
 def get_long_description():
     with open("README.md", 'r', encoding='utf-8') as f:
         return f.read()
 
 setup(
-    # TODO
-    name="MYNAME",  
-    # TODO
+    name="imposter",
     packages=[
-        "MYNAME"  
+        "imposter"
     ],
     include_package_data=True,
     license="MIT",
-    # TODO
-    description="MYDESCRIPTION",  
+    description="GET-POST request-response simulation",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
-    # TODO
-    url="myurl",  
-    # TODO
-    author="MYNAME",  
+    url="myurl",
+    author="imposter",
     setup_requires=["wheel"],
-    # TODO
     install_requires=[
     ],
     classifiers=[
@@ -32,10 +27,9 @@ setup(
     ],
     python_requires=">=3.7",
     version="0.0.1-beta",
-    # TODO
     entry_points="""
         [console_scripts]
-        gico = gico.gico:main 
+        imposter = imposter.app:main
     """
 )
 
